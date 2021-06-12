7bab513917ea6395675851ef425661a216ae1e39
LucaTheHacker <github@damettoluca.com>
2021-06-12 11:51:20 +0200

    Switched net/http -> fasthttp

--- disconnect.go
+++ disconnect.go
@@ -14,27 +14,33 @@ package GoFritzBox
 import (
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"time"
+
+	"github.com/valyala/fasthttp"
 )
 
 // Disconnect disconnects your Fritz!Box from the internet
 // This is usually used to change your IP address
 // The prodecure can require up to 30 seconds, after that the internet connection will be re-enabled
 func (s *SessionInfo) Disconnect() error {
-	url := fmt.Sprintf("%s/internet/inetstat_monitor.lua?sid=%s&myXhr=1&action=disconnect&useajax=1&xhr=1&t%d=nocache", s.EndPoint, s.SID, time.Now().Unix())
-	resp, err := http.Get(url)
+	request := fasthttp.AcquireRequest()
+	response := fasthttp.AcquireResponse()
+	defer func() {
+		fasthttp.ReleaseRequest(request)
+		fasthttp.ReleaseResponse(response)
+	}()
+
+	request.SetRequestURI(fmt.Sprintf(
+		"%s/internet/inetstat_monitor.lua?sid=%s&myXhr=1&action=disconnect&useajax=1&xhr=1&t%d=nocache",
+		s.EndPoint, s.SID, time.Now().Unix(),
+	))
+
+	err := client.Do(request, response)
 	if err != nil {
 		return err
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-
-	if string(body) == "done:0" {
+	if string(response.Body()) == "done:0" {
 		return nil
 	} else {
 		return errors.New("failed to disconnect")
